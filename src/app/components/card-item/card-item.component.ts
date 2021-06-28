efc81dfd4dbbd557151c24239c1c647474f7a27e
Matheus <matheushdealmeida@gmail.com>
2021-06-28 20:07:48 -0300

    Incluido logica para exibir imagens de base64

--- src/app/components/card-item/card-item.component.ts
+++ src/app/components/card-item/card-item.component.ts
@@ -14,4 +14,9 @@ export class CardItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getImageUrl(): string {
+    if (this.praca.picture.length <= 0 || this.praca.picture == '')
+      return '';
+    return 'data:image/jpeg;base64,' + this.praca.picture;
+  }
 }
